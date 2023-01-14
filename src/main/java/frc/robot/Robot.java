dd1083cf79a10b882eec0328cb3d2853772970de
Nick Plucker <pluckernick@gmail.com>
2023-01-14 11:42:03 -0800

    Add correct CAN deviceIds and add xbox controller

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
@@ -18,8 +19,6 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
  */
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
 
   private CANSparkMax m_leftMotorFront;
   private CANSparkMax m_leftMotorRear;
@@ -29,24 +28,33 @@ public class Robot extends TimedRobot {
   private MotorControllerGroup m_leftGroup;
   private MotorControllerGroup m_rightGroup;
 
+  private XboxController xboxController;
+
   @Override
   public void robotInit() {
-    m_leftMotorFront = new CANSparkMax(1, MotorType.kBrushless);
-    m_leftMotorRear = new CANSparkMax(2, MotorType.kBrushless);
-    m_rightMotorFront = new CANSparkMax(3, MotorType.kBrushless);
-    m_rightMotorRear = new CANSparkMax(4, MotorType.kBrushless);
+    // Assign each motor a CAN deviceId so it can be identified and a MotorType.
+    m_leftMotorFront = new CANSparkMax(6, MotorType.kBrushless);
+    m_leftMotorRear = new CANSparkMax(12, MotorType.kBrushless);
+    m_rightMotorFront = new CANSparkMax(8, MotorType.kBrushless);
+    m_rightMotorRear = new CANSparkMax(15, MotorType.kBrushless);
 
+    // Assign the motors to a group so they move in sync (robot uses 2 NEO motors per side)
     m_leftGroup = new MotorControllerGroup(m_leftMotorFront, m_leftMotorRear);
     m_rightGroup = new MotorControllerGroup(m_rightMotorFront, m_rightMotorRear);
 
+    // Create the differential drive code 
     m_myRobot = new DifferentialDrive(m_leftGroup, m_rightGroup);
 
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    // Invert one side so each side drives the same direction
+    m_leftGroup.setInverted(true);
+
+    // Assign the XboxController to USB Port 0 (can be found in driver station)
+    xboxController = new XboxController(0);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    // Declare tank drive system and assign a button for each side. In this example, it's the two joysticks on the Xbox controller on the Y axis. 
+    m_myRobot.tankDrive(xboxController.getLeftY(), xboxController.getRightY());
   }
 }
