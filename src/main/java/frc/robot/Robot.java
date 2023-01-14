30ecb00150495bcb708856c3cf10f798dbf4a197
Nick Plucker <pluckernick@gmail.com>
2023-01-14 11:09:40 -0600

    Swap PWM with CAN

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,13 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
@@ -19,23 +21,32 @@ public class Robot extends TimedRobot {
   private Joystick m_leftStick;
   private Joystick m_rightStick;
 
-  private final MotorController m_leftMotor = new PWMSparkMax(0);
-  private final MotorController m_rightMotor = new PWMSparkMax(1);
+  private CANSparkMax m_leftMotorFront;
+  private CANSparkMax m_leftMotorRear;
+  private CANSparkMax m_rightMotorFront;
+  private CANSparkMax m_rightMotorRear;
+
+  private MotorControllerGroup m_leftGroup;
+  private MotorControllerGroup m_rightGroup;
 
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    m_leftMotorFront = new CANSparkMax(1, MotorType.kBrushless);
+    m_leftMotorRear = new CANSparkMax(2, MotorType.kBrushless);
+    m_rightMotorFront = new CANSparkMax(3, MotorType.kBrushless);
+    m_rightMotorRear = new CANSparkMax(4, MotorType.kBrushless);
 
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    m_leftGroup = new MotorControllerGroup(m_leftMotorFront, m_leftMotorRear);
+    m_rightGroup = new MotorControllerGroup(m_rightMotorFront, m_rightMotorRear);
+
+    m_myRobot = new DifferentialDrive(m_leftGroup, m_rightGroup);
+
     m_leftStick = new Joystick(0);
     m_rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
   }
 }
